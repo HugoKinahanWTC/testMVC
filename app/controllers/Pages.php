238d8adecf2b7d011bd4b9ef84bd968f33bab937
HugoKinahan <hugokinahan@hotmail.com>
2022-02-04 15:52:07 +0000

    end of section 4 - framework set up

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -2,14 +2,13 @@
 
 class Pages extends Controller {
     public function __construct() {
-        $this->postModel = $this->model('Post');
+
     }
 
     public function index() {
-        $posts = $this->postModel->getPosts();
+
         $data = [
-            'title' => 'Welcome',
-            'posts' => $posts
+            'title' => 'Test MVC'
         ];
         $this->view('pages/index', $data);
     }
