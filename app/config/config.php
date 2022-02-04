b6ae1140224c757f7b5e9e68d70ad964f0cdea77
HugoKinahan <hugokinahan@hotmail.com>
2022-02-04 15:39:39 +0000

    initial commit

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '123456');
+define('DB_NAME', 'testMVC');
+
+// App Route
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// Url Route
+define('URLROOT', 'http://localhost/testMVC');
+
+// Site Name
+define('SITENAME', 'testMVC');
+
+
